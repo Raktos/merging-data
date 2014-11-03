c756114b6fb66a5d7f5b8731e604174257dca5ab
Information School <ischoolit@uw.edu>
2014-11-03 13:17:29 -0800

    static name and images

--- js/app.js
+++ js/app.js
@@ -5,3 +5,11 @@
     Add code here to create a new Angular application and controller.
     The array of employee objects is already in a global variable named 'pawneeEmployees'
 */
+
+"use strict";
+
+angular.module('employeeApp', [])
+    .controller('employeeController', function($scope) {
+        $scope.employeeList = pawneeEmployees;
+    }
+);
